f81cfd87af5e964188b256ccd4df1186c1c5ba31
Sadik Bakiu <sbakiu@gmail.com>
2022-04-21 07:30:43 +0200

    Add canary deployment

--- app/client.py
+++ app/client.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 from seldon_core.seldon_client import SeldonClient
 
 import numpy as np
@@ -17,16 +19,22 @@ import numpy as np
 sc = SeldonClient(
     gateway="ambassador",
     transport="rest",
-    gateway_endpoint="localhost:8003",  # Make sure you use the port above
+    gateway_endpoint="localhost:8080",  # Make sure you use the port above
     namespace="seldon",
 )
 
-client_prediction = sc.predict(
-    data=np.array(["Hello world this is a test"]),
-    deployment_name="abtest",
-    names=["text"],
-    payload_type="ndarray",
-)
+for i in range(100):
 
-print(client_prediction)
+    client_prediction = sc.predict(
+        data=np.array(["Hello world this is a test"]),
+        deployment_name="abtest",
+        names=["text"],
+        payload_type="ndarray",
+    )
+
+    if client_prediction.success:
+        print(client_prediction)
+    else:
+        print(client_prediction)
+    sleep(0.5)
 
