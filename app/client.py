79ff285837b1753b9002fe188a03c9a9b192c79f
Sadik Bakiu <sbakiu@gmail.com>
2022-05-01 21:39:39 +0200

    use different input

--- app/client.py
+++ app/client.py
@@ -1,5 +1,8 @@
+import logging
 from time import sleep
 
+import pandas as pd
+from sklearn.model_selection import train_test_split
 from seldon_core.seldon_client import SeldonClient
 
 import numpy as np
@@ -10,19 +13,34 @@ sc = SeldonClient(
     gateway_endpoint="localhost:8080",  # Make sure you use the port above
     namespace="seldon",
 )
+df_cols = ["prev_idx", "parent_idx", "body", "removed"]
 
-for i in range(1000):
+df = pd.read_csv(
+    "https://raw.githubusercontent.com/axsauze/reddit-classification-exploration/master/data/reddit_train.csv",
+    names=df_cols,
+    skiprows=1,
+    encoding="ISO-8859-1",
+)
+
+df.head()
+
+x = df["body"].values
+y = df["removed"].values
+logging.info("Train test split.")
+_, x_test, _, _ = train_test_split(
+    x, y, stratify=y, random_state=42, test_size=0.1, shuffle=True
+)
+
+for i in range(len(x_test)):
+    test_text = x_test[i]
 
     client_prediction = sc.predict(
-        data=np.array(["Hello world this is a test"]),
+        data=np.array([test_text]),
         deployment_name="abtest",
         names=["text"],
         payload_type="ndarray",
     )
 
-    if client_prediction.success:
-        print(client_prediction)
-    else:
-        print(client_prediction)
+    print(client_prediction)
     sleep(0.5)
 
