aac7c5bfdd05171475b82a241bd27f1081bbbec3
Sadik Bakiu <sbakiu@gmail.com>
2023-05-06 18:04:25 +0200

    Change streamlit version to 1.0.0

--- build-streamlit.sh
+++ build-streamlit.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 gcloud builds submit --config=cloudbuild.yaml \
-  --substitutions=_LOCATION="europe-west3",_REPOSITORY="ab-testing",_DOCKERFILE="Dockerfile.streamlit",_IMAGE="streamlit-app",_TAG="1.0.2" \
+  --substitutions=_LOCATION="europe-west3",_REPOSITORY="ab-testing",_DOCKERFILE="Dockerfile.streamlit",_IMAGE="streamlit-app",_TAG="1.0.0" \
   .
